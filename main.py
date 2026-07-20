446a1ff7806b988958e10b3d6ba215ff726c7c6b
Aakash Agrahari <aakashagrahari45@gmail.com>
2026-07-20 23:49:51 +0530

    backend changes related to notes

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from mcp.server.fastmcp import FastMCP
+import os
 
-# here we will initialize mcp server
+# here we will initialize mcp server, this is the entry point in the server, after this we can define tools and resources
 mcp = FastMCP("MyExampleServer")
 
-# defining a tool
+# defining a tool, this is the basic tool for greeting
 @mcp.tool()
 def greet(name: str) -> str:
     print('1')
     """Greets the user by name."""
     return f"Hello, {name}!"
 
-# defining a tool
+# defining a tool, this is also a basic tool for addition
 @mcp.tool()
 def add(a: int, b: int) -> int:
   print('2')
   """Adds two numbers."""
   return a + b
 
-# defining a resource
+# defining a resource, this is the basic tool for providing a description of the server
 @mcp.resource("info://description")
 def description() -> str:
     print('3')
@@ -26,3 +27,78 @@ def description() -> str:
 
 if __name__ == "__main__":
     mcp.run()
+
+
+#here we are intitializing a tool to seatch notes in the notes folder
+@mcp.tool()
+def search_notes(keyword: str) -> str:
+    """
+    Searches all text files inside the notes folder.
+    Returns every note containing the keyword.
+    """
+
+    notes_folder = "notes"
+
+    if not os.path.exists(notes_folder):
+        return "Notes folder not found."
+
+    results = []
+
+    for file in os.listdir(notes_folder):
+
+        if file.endswith(".txt"):
+
+            path = os.path.join(notes_folder, file)
+
+            with open(path, "r", encoding="utf-8") as f:
+                content = f.read()
+
+                if keyword.lower() in content.lower():
+                    results.append(
+                        f"File: {file}\n{content}\n"
+                    )
+
+    if not results:
+        return "No matching notes found."
+
+    return "\n-----------------\n".join(results)    
+
+
+#with this tool we can add a new note to the notes folder
+@mcp.tool()
+def add_note(title: str, content: str) -> str:
+    """
+    Creates a new note.
+    """
+
+    os.makedirs("notes", exist_ok=True)
+
+    filename = f"{title}.txt"
+
+    with open(
+        os.path.join("notes", filename),
+        "w",
+        encoding="utf-8"
+    ) as f:
+        f.write(content)
+
+    return f"Note '{title}' created successfully."
+
+
+
+# this tool will delete a note from the notes folder
+@mcp.tool()
+def delete_note(title: str) ->str:
+    """
+    Deletes a note.
+    """
+
+    path = os.path.join("notes", f"{title}.txt")
+
+    if not os.path.exists(path):
+        return "Note not found."
+
+    os.remove(path)
+
+    return "Note deleted successfully."
+
